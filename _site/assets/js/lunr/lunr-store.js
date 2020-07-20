95077627545560e0b0376d811218247522b39660
ethulia <minglu90@gmail.com>
2020-07-19 19:27:15 -0700

    remove some files, change some stuff

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "From Pork Bellies To Product Management",
+        "excerpt":"Aside from the great learning experience and amazing friends that I got from my first job after college, I also get to tell people that in a past life, I was a commodities trader (agriculture futures to be exact, e.g. corn, soy, hogs, cattle). It always causes raised eyebrows, being...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/from-pork-bellies-to-product-management/",
+        "teaser": null
+      }]
